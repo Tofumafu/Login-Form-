348a99f62e419e615f21dac0818f3eff1a0d251e
Tofu <154775782+Tofumafu@users.noreply.github.com>
2025-01-02 21:42:50 +1030

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,16 @@ const wrapper = document.querySelector('.wrapper');
 const loginlink = document.querySelector('.login-link');
 const signuplink = document.querySelector('.signup-link');
 
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+
+document.getElementById("login").onclick = function() {
+    email = document.getElementById('email').value;
+    password = document.getElementById('password').value;
+
+    console.log(`Email: ${email}`);
+    console.log(`Password: ${password}`);
+}
 signuplink.addEventListener('click', () => {
     wrapper.classList.add('active');
 });
